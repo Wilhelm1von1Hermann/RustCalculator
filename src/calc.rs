b792594a57d2d97825251288782d09c3c5163054
Wilhelm von Hermann <106443785+Wilhelm1von1Hermann@users.noreply.github.com>
2025-05-02 12:18:09 +0300

    v0.0.2

--- src/calc.rs
+++ src/calc.rs
@@ -1,25 +1,25 @@
 // this is where the calculator logic is implemented
 
 #![allow(dead_code)]
-use clearscreen;
+use clearscreen::{self};
 
 fn main() {
     println!("I feel like you should use other functions.");
 }
 
-pub fn plus(a: i32, b: i32) { // plus function
+pub fn plus(a: f32, b: f32) { // plus function
     clearscreen::clear().unwrap();
     println!("plus() result: {}", a + b);
     print!("\n"); // new line
 }
 
-pub fn minus(a: i32, b: i32) { // minus function
+pub fn minus(a: f32, b: f32) { // minus function
     clearscreen::clear().unwrap();
     println!("minus() result: {}", a - b);
     print!("\n"); // new line
 }
 
-pub fn multiplication(a: i32, b: i32) { // multiplication function
+pub fn multiplication(a: f32, b: f32) { // multiplication function
     clearscreen::clear().unwrap();
     println!("multiplication() result: {}", a * b);
     print!("\n"); // new line
@@ -37,5 +37,42 @@ pub fn division(a: f32, b: f32) -> u8 { // division function
         println!("division() result: {}", a / b);
         print!("\n"); // new line
         return 0;
+    }
+}
+
+pub fn remainder(a: f32, b: f32) { // remainder function
+    clearscreen::clear().unwrap();
+    println!("remainder() result: {}", a % b);
+    print!("\n");
+}
+
+pub fn squares(side: f32) -> u8 { // square's area and circumference function
+    clearscreen::clear().unwrap();
+    if side > 0.0 {
+        println!("squares() AREA result: {}", side*side * 4.0);
+        println!("squares() CIRCUMFERENCE result: {}", side * 4.0);
+        print!("\n");
+        return 0;
+    }
+    else {
+        println!("The side length is smaller than 0. Can't proceed.");
+        print!("\n");
+        return 1;
+    }
+}
+
+pub fn circles(radius: f32) -> u8 { // circle's area and circumference function
+    clearscreen::clear().unwrap();
+    const PI: f32 = 3.1415926;
+    if radius > 0.0 {
+        println!("circles() AREA result: {}", radius*radius * PI);
+        println!("circles() CIRCUMFERENCE result: {}", radius * PI);
+        print!("\n");
+        return 0;
+    }
+    else {
+        println!("Radius set to 0 or less. Can't proceed.");
+        print!("\n");
+        return 1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,180 @@
-// this is just the interface and some small logic
-mod calc; // importing calc.rs for logic
+// this is just the interface and its logic
+
+mod calc; // importing calc.rs for the calculator logic
 
 fn main() { // boots up the calculator
-    let ver = "2025-05-01 v0.0.1";
-    println!("main() active, welcome to Rust.");
+    let ver = "2025-05-02 v0.0.2";
+    println!("main() active, glory to Rust.");
     println!("CALC VERSION - {}", ver);
     calc();
 }
 
 fn calc() { // the cli and some parts of the calculator
     use std::io::stdin;
+    use inline_colorization::*;
 
-    println!("\n \n"); // skips two lines after main()
-    println!("Select your operation.");
-    println!("1 - Plus; 2 - Minus; 3 - Multiplication; 4 - Division; 0 - EXIT."); // all variants are here
-    const EXIT_LINE: &str = "All good! Thanks for using the calculator.";
+    println!("\n \n"); // skips two lines after main() or failing
+    println!("{style_bold}Select your operation.{style_reset}"); // bold
+    println!("{style_bold}1 - Plus (+); 2 - Minus (-); 3 - Multiplication (*); 4 - Division (/); 5 - Remainder (%); 0 - EXIT.{style_reset}"); // all variants are here
+    const EXIT_LINE: &str = "Thanks for using the calculator.";
     let mut user_input = String::new();
-    stdin().read_line(&mut user_input).expect("Failed to read line");
+    stdin().read_line(&mut user_input).expect("Failed.");
 
-    let input = user_input.trim();
+    let input = match user_input.trim().parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("{color_red}Failed. You need a number!{color_reset}");
+            return calc();
+        }
+    };
 
-    if input == "1" { // plus
+    if input == 1 { // plus
         println!("Plus selected! First number:");
         let mut a = String::new();
         stdin().read_line(&mut a).expect("fail.");
-        let a: i32 = a.trim().parse().expect("fail. you need a number!");
+        let a = match a.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
+
         println!("Second number:");
         let mut b = String::new();
         stdin().read_line(&mut b).expect("fail.");
-        let b: i32 = b.trim().parse().expect("fail. you need a number!");
+        let b: f32 = match b.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
 
         calc::plus(a, b);
-        println!("{}", EXIT_LINE);
-        std::io::stdin().read_line(&mut String::new()).unwrap();
+        println!("{color_bright_blue}{EXIT_LINE}{color_reset}");
+        calc();
     }
-    else if input == "2" { // minus
+
+    else if input == 2 { // minus
         println!("Minus selected! Your first number:");
         let mut a = String::new();
         stdin().read_line(&mut a).expect("fail.");
-        let a: i32 = a.trim().parse().expect("fail. you need a number!");
+        let a: f32 = match a.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
         println!("Second number:");
         let mut b = String::new();
         stdin().read_line(&mut b).expect("fail.");
-        let b: i32 = b.trim().parse().expect("fail. you need a number!");
+        let b: f32 = match b.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
 
         calc::minus(a, b);
-        println!("{}", EXIT_LINE);
-        std::io::stdin().read_line(&mut String::new()).unwrap();
+        println!("{color_bright_blue}{EXIT_LINE}{color_reset}");
+        calc();
     }
-    else if input == "3" { // multiplication
+
+    else if input == 3 { // multiplication
         println!("Multiplication selected! Your first number:");
         let mut a = String::new();
         stdin().read_line(&mut a).expect("fail.");
-        let a: i32 = a.trim().parse().expect("fail. you need a number!");
+        let a: f32 = match a.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
         println!("Second number:");
         let mut b = String::new();
         stdin().read_line(&mut b).expect("fail.");
-        let b: i32 = b.trim().parse().expect("fail. you need a number!");
+        let b: f32 = match b.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
 
         calc::multiplication(a, b);
-        println!("{}", EXIT_LINE);
-        std::io::stdin().read_line(&mut String::new()).unwrap();
+        println!("{color_bright_blue}{EXIT_LINE}{color_reset}");
+        calc();
     }
-    else if input == "4" { // division
+
+    else if input == 4 { // division
         println!("Division selected. Your first number:");
         let mut a = String::new();
         stdin().read_line(&mut a).expect("fail.");
-        let a: f32 = a.trim().parse().expect("fail. you need a number!");
+        let a: f32 = match a.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
         println!("Second number:");
         let mut b = String::new();
         stdin().read_line(&mut b).expect("fail.");
-        let b: f32 = b.trim().parse().expect("fail. you need a number!");
+        let b: f32 = match b.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number!{color_reset}");
+                return calc();
+            }
+        };
 
         calc::division(a, b);
         let n: u8 = calc::division(a, b);
         if n == 1 {
-            println!("I'm sorry? Dividing by zero? Stonks.");
+            println!("{}", "I'm sorry? Dividing by zero? Stonks.");
+            calc();
         }
         else {
-            println!("{}", EXIT_LINE);
+            println!("{color_bright_blue}{EXIT_LINE}{color_reset}");
         }
-        std::io::stdin().read_line(&mut String::new()).unwrap();
+        calc();
     }
-    else if input == "0" {
 
+    else if input == 5 { // remainder function
+        println!("Remainder selected. Your first number:");
+        let mut a = String::new();
+        stdin().read_line(&mut a).unwrap();
+        let a: f32 = match a.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number.{color_reset}");
+                return calc();
+            }
+        };
+        println!("Second number:");
+        let mut b = String::new();
+        stdin().read_line(&mut b).unwrap();
+        let b: f32 = match b.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{color_red}Failed. You need a number.{color_reset}");
+                return calc();
+            }
+        };
+
+        calc::remainder(a, b);
+        println!("{color_bright_blue}{EXIT_LINE}{color_reset}");
+        calc();
     }
+
+    else if input == 0 {
+        println!("Exiting... 1337");
+    }
+
     else {
         println!("Hasn't been able to find an operator of that kind. Try again.");
         calc();
